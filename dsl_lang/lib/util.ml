80c78c0aed9d11685375bd4971bf146b06fa3009
Anshuman Mohan <anshumanmohan@live.com>
2024-07-25 17:19:21 -0400

    Dune fmt

--- dsl_lang/lib/util.ml
+++ dsl_lang/lib/util.ml
@@ -36,10 +36,11 @@ let rec string_of_plist (plist : Ast.policy list) : string =
   | h :: t -> string_of_policy h ^ ", " ^ string_of_plist t
 
 (** Takes a policy and returns the string representation of it. **)
-and string_of_policy (pol : Ast.policy) : string =
+and
+string_of_policy (pol : Ast.policy) : string =
   match pol with
   | Class c -> c
-    | Fifo pl -> "fifo[" ^ string_of_plist pl ^ "]"
-    | Strict pl -> "strict[" ^ string_of_plist pl ^ "]"
-    | RoundRobin pl -> "rr[" ^ string_of_plist pl ^ "]"
-    | Var v -> v
+  | Fifo pl -> "fifo[" ^ string_of_plist pl ^ "]"
+  | Strict pl -> "strict[" ^ string_of_plist pl ^ "]"
+  | RoundRobin pl -> "rr[" ^ string_of_plist pl ^ "]"
+  | Var v -> v
