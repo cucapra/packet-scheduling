66c2a4edcf546e38be99c5171d30fbbb29edfd34
Anshuman Mohan <anshumanmohan@live.com>
2024-07-25 16:15:11 -0400

    Try ocamlformat

--- dsl_lang/lib/ast.ml
+++ dsl_lang/lib/ast.ml
@@ -2,16 +2,13 @@ type clss = string
 type var = string
 
 type policy =
-| Class of clss
-| Fifo of policy list
-| Fair of policy list
-| Strict of policy list
-| Var of var
+  | Class of clss
+  | Fifo of policy list
+  | Fair of policy list
+  | Strict of policy list
+  | Var of var
 
 type declare = clss list
-
 type assignment = var * policy
-
 type return = policy
-
-type program = declare * (assignment list) * return
+type program = declare * assignment list * return
