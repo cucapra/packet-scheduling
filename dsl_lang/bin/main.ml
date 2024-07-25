ae626db89dcc7f68a47dae774fef1559237b4af7
Kabir Samsi <samsikabir@gmail.com>
2024-07-25 18:54:34 -0400

    Added pretty-printing and testing suppor for new algortihms

--- dsl_lang/bin/main.ml
+++ dsl_lang/bin/main.ml
@@ -44,6 +44,14 @@ let tests =
       "strict[A, B, rr[rr[CU, CV], strict[CW, CX]]]";
     make_test "strict of 3" "../../dsl/progs/soon/strict_n_classes.sched"
       "strict[A, B, C]";
+    make_test "leaky bucket of 2" "../../dsl/progs/nwc/leaky_2_classes.sched"
+      "leaky[[A, B, width = 20, time = 50]]";
+    make_test "token bucket of 2 round robins" "../../dsl/progs/soon/token_2_rr_children.sched"
+      "token[[rr[A, B], rr[C, D]], width = 20, time = 50]";
+    make_test "stop and go with 3 classes" "../../dsl/progs/soon/sg_3_classes.sched"
+      "stopandgo[[stopandgo[[A, B], 10], stopandgo[[C], 10], 5]";
+    make_test "rcsp for 4 classes" "../../dsl/progs/soon/rcsp_4_classes.sched"
+      "rcsp[A, B, C, D]";
 
   ]
 
--- dsl_lang/lib/util.ml
+++ dsl_lang/lib/util.ml
@@ -1,24 +1,44 @@
 exception NonTerminal
 exception ParserError of string
 
-(** Helper function to turn policy lists into strings. **)
-let rec string_of_plist (plist : Ast.policy list) : string =
-  match plist with
-  | [] -> ""
-  | [x] -> string_of_policy x
-  | h :: t -> string_of_policy h ^ ", " ^ string_of_plist t
+(* Helper function to compactly join policy lists by comma *)
+let rec join = List.fold_left (fun acc s ->
+  let parsed = string_of_policy s in
+  if acc = "" then parsed
+  else (acc ^ ", " ^ parsed)) ""
+
+(* Helper function to compactly join weighted policy lists by comma *)
+and join_weighted = List.fold_left (fun acc s ->
+  let parsed = "(" ^ string_of_policy (fst s) ^ ", " ^ string_of_int (snd s) ^ ")" in
+  if acc = "" then parsed
+  else (acc ^ ", " ^ parsed)) ""
 
 (** Takes a policy and returns the string representation of it. **)
 and string_of_policy (pol : Ast.policy) : string =
   match pol with
   | Class c -> c
-  | Fifo [x] -> "fifo[" ^ string_of_policy x ^ "]"
-  | Strict [x] -> "strict[" ^ string_of_policy x ^ "]"
-  | RoundRobin [x] -> "rr[" ^ string_of_policy x ^ "]"
-  | Fifo (h :: t) ->
-      "fifo[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
-  | Strict (h :: t) ->
-      "strict[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
-  | RoundRobin (h :: t) ->
-      "rr[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+  | Fifo lst -> "fifo[" ^ join lst ^ "]"
+  | RoundRobin lst -> "rr[" ^ join lst ^ "]"
+  | Strict lst -> "strict[" ^ join lst ^ "]"
+  | WeightedFair lst -> "strict[" ^ join_weighted lst ^ "]"
+
+  | EarliestDeadline lst -> "edf[" ^ join lst ^ "]"
+  | ShortestJobNext lst -> "sjn[" ^ join lst ^ "]"
+  | ShortestRemaining lst -> "srtf[" ^ join lst ^ "]"
+  | RateControlled lst -> "rcsp[" ^ join lst ^ "]"
+
+  | LeakyBucket (lst, weight, buffer) ->
+      "leaky[[" ^ join list ^ "], weight = "
+      ^ string_of_int weight ^ ", buffer = "
+      ^ string_of_int buffer ^ "]"
+
+  | TokenBucket (lst, weight, buffer) ->
+      "token[[" ^ join list ^ "], weight = "
+      ^ string_of_int weight ^ ", buffer = "
+      ^ string_of_int buffer ^ "]"
+
+  | StopAndGo (lst, weight) -> 
+      "stopandgo[[" ^ join lst ^ "], weight = "
+      ^ string_of_int weight ^ "]"
+
   | _ -> "error"
