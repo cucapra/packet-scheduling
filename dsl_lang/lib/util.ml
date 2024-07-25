df2972575219584f95a01cbd3b75a601a586457f
Anshuman Mohan <anshumanmohan@live.com>
2024-07-25 17:07:54 -0400

    Simplify string_of_policy

--- dsl_lang/lib/util.ml
+++ dsl_lang/lib/util.ml
@@ -36,16 +36,11 @@ let rec string_of_plist (plist : Ast.policy list) : string =
   | h :: t -> string_of_policy h ^ ", " ^ string_of_plist t
 
 (** Takes a policy and returns the string representation of it. **)
-and string_of_policy (pol : Ast.policy) : string =
-  match pol with
-  | Class c -> c
-  | Fifo [x] -> "fifo[" ^ string_of_policy x ^ "]"
-  | Strict [x] -> "strict[" ^ string_of_policy x ^ "]"
-  | Fair [x] -> "rr[" ^ string_of_policy x ^ "]"
-  | Fifo (h :: t) ->
-      "fifo[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
-  | Strict (h :: t) ->
-      "strict[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
-  | Fair (h :: t) ->
-      "rr[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
-  | _ -> "error"
+and
+string_of_policy (pol : Ast.policy) : string =
+    match pol with
+    | Class c -> c
+    | Fifo pl -> "fifo[" ^ string_of_plist pl ^ "]"
+    | Strict pl -> "strict[" ^ string_of_plist pl ^ "]"
+    | Fair pl -> "rr[" ^ string_of_plist pl ^ "]"
+    | Var v -> v
