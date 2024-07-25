faf97785322b5ce02a0184da7fd7b2e22a3543a4
Cassandra Sziklai <cns58@cornell.edu>
2024-07-25 16:51:25 -0400

    update formatting

--- dsl_lang/bin/main.ml
+++ dsl_lang/bin/main.ml
@@ -1,16 +1,7 @@
 open Dsl_core
 open OUnit2
 
-(* The top-level entry point for running our interpreter. *)
-(* Main function. *)
-(* let () =
-   let _ =
-     if Array.length Sys.argv <> 2 then
-       (Printf.printf "Usage: run interpreter <file>\n";
-        exit 0) in
-   let filename = Sys.argv.(1) in
-   let res =
-   Util.parse_file filename in ignore (Eval.eval res) *)
+(* The test suite for running our interpreter. *)
 
 let eval_prog (filename : string) =
   let res = Util.parse_file filename in
@@ -22,8 +13,7 @@ let make_test (name : string) (filename : string) (val_str : string) =
     (Util.string_of_policy (eval_prog filename))
     ~printer:(fun x -> x)
 
-let make_error_test (name : string) (filename : string) =
-  let exn = Eval.UnboundVariable "..." in
+let make_error_test (name : string) (filename : string) (exn : exn ) =
   name >:: fun _ ->
   assert_raises exn (fun () ->
       try Util.string_of_policy (eval_prog filename)
@@ -36,36 +26,34 @@ let tests =
       "../../dsl/progs/now/fifo_1_class_sugar.sched" "A";
     make_test "fifo 1 class" "../../dsl/progs/now/fifo_1_class.sched" "A";
     make_test "fifo of 3" "../../dsl/progs/now/fifo_n_classes.sched"
-      "fifo[ A, B, C, ]";
-    make_test "rr of 1" "../../dsl/progs/now/rr_1_class.sched" "rr[ A, ]";
-    make_test "rr of 2" "../../dsl/progs/now/rr_2_classes.sched" "rr[ A, B, ]";
+      "fifo[A, B, C]";
+    make_test "rr of 1" "../../dsl/progs/now/rr_1_class.sched" "rr[A]";
+    make_test "rr of 2" "../../dsl/progs/now/rr_2_classes.sched" "rr[A, B]";
     make_test "multiple assignments"
       "../../dsl/progs/now/rr_hier_merge_sugar.sched"
-      "rr[ fifo[ BX, BY, ], rr[ RP, RT, ], ]";
+      "rr[fifo[BX, BY], rr[RP, RT]]";
     make_test "2 assignments w/ substitutions"
-      "../../dsl/progs/now/rr_hier.sched" "rr[ B, rr[ RP, RT, ], ]";
+      "../../dsl/progs/now/rr_hier.sched" "rr[B, rr[RP, RT]]";
     make_test "3 classes with substitutions"
       "../../dsl/progs/soon/rr_n_class_hier.sched"
-      "rr[ A, B, rr[ rr[ CU, CV, ], rr[ CW, CX, ], ], ]";
+      "rr[A, B, rr[rr[CU, CV], rr[CW, CX]]]";
     make_test "rr of 3" "../../dsl/progs/soon/rr_n_classes.sched"
-      "rr[ A, B, C, ]";
+      "rr[A, B, C]";
     make_test "rr and strict substitutions"
       "../../dsl/progs/soon/rr_strict_n_classes_hier.sched"
-      "strict[ A, B, rr[ rr[ CU, CV, ], strict[ CW, CX, ], ], ]";
+      "strict[A, B, rr[rr[CU, CV], strict[CW, CX]]]";
     make_test "strict of 3" "../../dsl/progs/soon/strict_n_classes.sched"
-      "strict[ A, B, C, ]";
+      "strict[A, B, C]";
   ]
 
 let error_tests =
   [
     make_error_test "undeclared class"
-      "../../dsl/progs/incorrect/undeclared_classes.sched";
+      "../../dsl/progs/incorrect/undeclared_classes.sched" (Eval.UnboundVariable "...");
     make_error_test "unbound variable"
-      "../../dsl/progs/incorrect/unbound_var.sched";
+      "../../dsl/progs/incorrect/unbound_var.sched" (Eval.UnboundVariable "...");
     make_error_test "unbound var in middle of list of assignments"
-      "../../dsl/progs/incorrect/unbound_var_hier.sched";
-    make_error_test "unbound var at end of list of assignments"
-      "../../dsl/progs/incorrect/unbound_var_hier2.sched";
+      "../../dsl/progs/incorrect/unbound_var_hier.sched" (Eval.UnboundVariable "...");
   ]
 
 let suite = "suite" >::: tests @ error_tests
--- dsl_lang/lib/util.ml
+++ dsl_lang/lib/util.ml
@@ -32,16 +32,20 @@ let parse_file (f : string) =
 let rec string_of_plist (plist : Ast.policy list) : string =
   match plist with
   | [] -> ""
+  | [x] -> string_of_policy x
   | h :: t -> string_of_policy h ^ ", " ^ string_of_plist t
 
 (** Takes a policy and returns the string representation of it. **)
 and string_of_policy (pol : Ast.policy) : string =
   match pol with
   | Class c -> c
+  | Fifo [x] -> "fifo[" ^ string_of_policy x ^ "]"
+  | Strict [x] -> "strict[" ^ string_of_policy x ^ "]"
+  | Fair [x] -> "rr[" ^ string_of_policy x ^ "]"
   | Fifo (h :: t) ->
-      "fifo[ " ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+      "fifo[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
   | Strict (h :: t) ->
-      "strict[ " ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+      "strict[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
   | Fair (h :: t) ->
-      "rr[ " ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+      "rr[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
   | _ -> "error"
