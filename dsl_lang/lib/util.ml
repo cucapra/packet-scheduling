e87ef4ca7dde76e71ada8f9130375fe41e8e4767
Kabir Samsi <samsikabir@gmail.com>
2024-07-25 17:15:53 -0400

    Pre-merge updates to util file

--- dsl_lang/lib/util.ml
+++ dsl_lang/lib/util.ml
@@ -1,23 +1,24 @@
 exception NonTerminal
 exception ParserError of string
 
-(** syntax_error_msg lexbuf is a syntax error message for the current position **)
-let syntax_error_msg lexbuf =
-    let pos = Lexing.lexeme_start_p lexbuf in
-    let lnum, cnum = pos.pos_lnum, (pos.pos_cnum - pos.pos_bol) in
-    Printf.sprintf "Syntax error at line %d, character %d" lnum cnum
+(** Helper function to turn policy lists into strings. **)
+let rec string_of_plist (plist : Ast.policy list) : string =
+  match plist with
+  | [] -> ""
+  | [x] -> string_of_policy x
+  | h :: t -> string_of_policy h ^ ", " ^ string_of_plist t
 
-let parse lexbuf = Parser.prog Lexer.token lexbuf
-
-(** parse s parses a program string into an AST **)
-let parse_string (s : string) =
-  let lexbuf = Lexing.from_string s in 
-  try parse lexbuf with
-    | Parser.Error -> raise (ParserError (syntax_error_msg lexbuf))
-
-(** parse s parses a program file into an AST **)
-let parse_file (f : string) =
-  let lexbuf = Lexing.from_channel (open_in f) in 
-  try parse lexbuf with
-    | Parser.Error -> prerr_endline (syntax_error_msg lexbuf); exit 1
-    | Lexer.Err -> prerr_endline (syntax_error_msg lexbuf); exit 1
+(** Takes a policy and returns the string representation of it. **)
+and string_of_policy (pol : Ast.policy) : string =
+  match pol with
+  | Class c -> c
+  | Fifo [x] -> "fifo[" ^ string_of_policy x ^ "]"
+  | Strict [x] -> "strict[" ^ string_of_policy x ^ "]"
+  | RoundRobin [x] -> "rr[" ^ string_of_policy x ^ "]"
+  | Fifo (h :: t) ->
+      "fifo[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+  | Strict (h :: t) ->
+      "strict[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+  | RoundRobin (h :: t) ->
+      "rr[" ^ string_of_policy h ^ ", " ^ string_of_plist t ^ "]"
+  | _ -> "error"
