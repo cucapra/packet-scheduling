2d44022c5b6a69b0c70976bdbaebb2fca29b0955
polybeandip <akashdhiraj2019@gmail.com>
2024-07-26 14:11:36 -0400

    dune fmt

--- dsl_lang/lib/eval.ml
+++ dsl_lang/lib/eval.ml
@@ -24,15 +24,14 @@ let rec lookup s x : policy =
 
 (* Helper function that evaulates a policy list. *)
 let rec eval_plist (pl : policy list) (st : store) (cl : classes) =
-  match pl with
-  | [] -> pl
-  | h :: t -> eval_pol h st cl :: eval_plist t st cl
+  match pl with [] -> pl | h :: t -> eval_pol h st cl :: eval_plist t st cl
 
 (* Helper function that evaluates a weighted policy list. *)
 and eval_weighted_plist (pl : (policy * int) list) (st : store) (cl : classes) =
   match pl with
   | [] -> pl
-  | (pol, weight) :: t -> (eval_pol pol st cl, weight) :: eval_weighted_plist t st cl
+  | (pol, weight) :: t ->
+      (eval_pol pol st cl, weight) :: eval_weighted_plist t st cl
 
 (* Evaluates a policy, looking up any variables and substituting them in. *)
 and eval_pol (p : policy) (st : store) (cl : classes) : policy =
@@ -46,25 +45,25 @@ and eval_pol (p : policy) (st : store) (cl : classes) : policy =
   | Fifo (h :: t) -> Fifo (eval_pol h st cl :: eval_plist t st cl)
   | RoundRobin (h :: t) -> RoundRobin (eval_pol h st cl :: eval_plist t st cl)
   | Strict (h :: t) -> Strict (eval_pol h st cl :: eval_plist t st cl)
-  | WeightedFair((pol, weight) :: t)
-    -> WeightedFair(((eval_pol pol st cl), weight) :: (eval_weighted_plist t st cl))
-  | EarliestDeadline (h :: t)
-    -> EarliestDeadline (eval_pol h st cl :: eval_plist t st cl)
-  | ShortestJobNext (h :: t)
-    -> ShortestJobNext (eval_pol h st cl :: eval_plist t st cl)
-  | ShortestRemaining (h :: t)
-    -> ShortestRemaining (eval_pol h st cl :: eval_plist t st cl)
-  | RateControlled (h :: t)
-    -> RateControlled (eval_pol h st cl :: eval_plist t st cl)
-  | LeakyBucket(lst, n1, n2) -> LeakyBucket((eval_plist lst st cl), n1, n2)
-  | TokenBucket(lst, n1, n2) -> TokenBucket((eval_plist lst st cl), n1, n2)
-  | StopAndGo(lst, n) -> StopAndGo((eval_plist lst st cl), n)
-
+  | WeightedFair ((pol, weight) :: t) ->
+      WeightedFair ((eval_pol pol st cl, weight) :: eval_weighted_plist t st cl)
+  | EarliestDeadline (h :: t) ->
+      EarliestDeadline (eval_pol h st cl :: eval_plist t st cl)
+  | ShortestJobNext (h :: t) ->
+      ShortestJobNext (eval_pol h st cl :: eval_plist t st cl)
+  | ShortestRemaining (h :: t) ->
+      ShortestRemaining (eval_pol h st cl :: eval_plist t st cl)
+  | RateControlled (h :: t) ->
+      RateControlled (eval_pol h st cl :: eval_plist t st cl)
+  | LeakyBucket (lst, n1, n2) -> LeakyBucket (eval_plist lst st cl, n1, n2)
+  | TokenBucket (lst, n1, n2) -> TokenBucket (eval_plist lst st cl, n1, n2)
+  | StopAndGo (lst, n) -> StopAndGo (eval_plist lst st cl, n)
   | _ -> failwith "cannot have empty policy"
 
 (* A function to evaluate all the assignments in a program by updating the store
    with the variable and the policy it maps to. *)
-let rec eval_assn (alist : assignment list) (st : store) (cl : classes) : store =
+let rec eval_assn (alist : assignment list) (st : store) (cl : classes) : store
+    =
   match alist with
   | [] -> st
   | (var, pol) :: t ->
@@ -76,7 +75,7 @@ let rec eval_assn (alist : assignment list) (st : store) (cl : classes) : store 
 let eval_helper (prog : program) (st : store) (cl : classes) : policy =
   let clist, alist, ret = prog in
   let cl' = cl @ clist in
-  let st' = eval_assn alist st cl' in 
+  let st' = eval_assn alist st cl' in
   eval_pol ret st' cl'
 
 (* Outermost function that is called by main.ml *)
--- dsl_lang/lib/util.ml
+++ dsl_lang/lib/util.ml
@@ -3,15 +3,16 @@ exception ParserError of string
 
 (** Takes a policy and returns the string representation of it. **)
 let rec string_of_policy (pol : Ast.policy) : string =
-  
   (* Helper function to compactly join policy lists by comma *)
   let join lst = lst |> List.map string_of_policy |> String.concat ", " in
 
   (* Helper function to compactly join weighted policy lists by comma *)
-  let join_weighted lst = lst
-    |> List.map
-        (fun (x, y) -> "(" ^ string_of_policy x ^ ", " ^ string_of_int y ^ ")")
-    |> String.concat ", " in
+  let join_weighted lst =
+    lst
+    |> List.map (fun (x, y) ->
+           "(" ^ string_of_policy x ^ ", " ^ string_of_int y ^ ")")
+    |> String.concat ", "
+  in
 
   match pol with
   | Class c -> c
@@ -19,24 +20,16 @@ let rec string_of_policy (pol : Ast.policy) : string =
   | RoundRobin lst -> "rr[" ^ join lst ^ "]"
   | Strict lst -> "strict[" ^ join lst ^ "]"
   | WeightedFair lst -> "strict[" ^ join_weighted lst ^ "]"
-
   | EarliestDeadline lst -> "edf[" ^ join lst ^ "]"
   | ShortestJobNext lst -> "sjn[" ^ join lst ^ "]"
   | ShortestRemaining lst -> "srtf[" ^ join lst ^ "]"
   | RateControlled lst -> "rcsp[" ^ join lst ^ "]"
-
   | LeakyBucket (lst, width, buffer) ->
-      "leaky[[" ^ join lst ^ "], width = "
-      ^ string_of_int width ^ ", buffer = "
+      "leaky[[" ^ join lst ^ "], width = " ^ string_of_int width ^ ", buffer = "
       ^ string_of_int buffer ^ "]"
-
   | TokenBucket (lst, width, buffer) ->
-      "token[[" ^ join lst ^ "], width = "
-      ^ string_of_int width ^ ", time = "
+      "token[[" ^ join lst ^ "], width = " ^ string_of_int width ^ ", time = "
       ^ string_of_int buffer ^ "]"
-
-  | StopAndGo (lst, width) -> 
-      "stopandgo[[" ^ join lst ^ "], width = "
-      ^ string_of_int width ^ "]"
-  
-  | Var _ -> raise(NonTerminal)
+  | StopAndGo (lst, width) ->
+      "stopandgo[[" ^ join lst ^ "], width = " ^ string_of_int width ^ "]"
+  | Var _ -> raise NonTerminal
