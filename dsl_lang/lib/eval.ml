0e31edcd5858e561ccca114cb76d3ff135d0ed7e
polybeandip <akashdhiraj2019@gmail.com>
2024-07-27 00:07:27 -0400

    Swap order of args in eval_pol

--- dsl_lang/lib/eval.ml
+++ dsl_lang/lib/eval.ml
@@ -12,29 +12,29 @@ let lookup s x =
   | None -> raise (UnboundVariable x)
 
 (* Evaluates a policy, looking up any variables and substituting them in. *)
-let rec eval_pol st cl p =
+let rec eval_pol cl st p =
   (* Helper function that evaulates a policy list. *)
-  let eval_plst st cl = List.map (eval_pol st cl) in
+  let eval_plst cl st = List.map (eval_pol cl st) in
 
   (* Helper function that evaluates a weighted policy list. *)
-  let eval_weighted_plst st cl =
-    List.map (fun (x, i) -> (eval_pol st cl x, i))
+  let eval_weighted_plst cl st =
+    List.map (fun (x, i) -> (eval_pol cl st x, i))
   in
 
   match p with
   | Class c -> if List.mem c cl then p else raise (UndeclaredClass c)
-  | Var x -> eval_pol st cl (lookup st x)
-  | Fifo plst -> Fifo (eval_plst st cl plst)
-  | RoundRobin plst -> RoundRobin (eval_plst st cl plst)
-  | Strict plst -> Strict (eval_plst st cl plst)
-  | WeightedFair wplst -> WeightedFair (eval_weighted_plst st cl wplst)
-  | EarliestDeadline plst -> EarliestDeadline (eval_plst st cl plst)
-  | ShortestJobNext plst -> ShortestJobNext (eval_plst st cl plst)
-  | ShortestRemaining plst -> ShortestRemaining (eval_plst st cl plst)
-  | RateControlled plst -> RateControlled (eval_plst st cl plst)
-  | LeakyBucket (plst, n1, n2) -> LeakyBucket (eval_plst st cl plst, n1, n2)
-  | TokenBucket (plst, n1, n2) -> TokenBucket (eval_plst st cl plst, n1, n2)
-  | StopAndGo (plst, n) -> StopAndGo (eval_plst st cl plst, n)
+  | Var x -> eval_pol cl st (lookup st x)
+  | Fifo plst -> Fifo (eval_plst cl st plst)
+  | RoundRobin plst -> RoundRobin (eval_plst cl st plst)
+  | Strict plst -> Strict (eval_plst cl st plst)
+  | WeightedFair wplst -> WeightedFair (eval_weighted_plst cl st wplst)
+  | EarliestDeadline plst -> EarliestDeadline (eval_plst cl st plst)
+  | ShortestJobNext plst -> ShortestJobNext (eval_plst cl st plst)
+  | ShortestRemaining plst -> ShortestRemaining (eval_plst cl st plst)
+  | RateControlled plst -> RateControlled (eval_plst cl st plst)
+  | LeakyBucket (plst, n1, n2) -> LeakyBucket (eval_plst cl st plst, n1, n2)
+  | TokenBucket (plst, n1, n2) -> TokenBucket (eval_plst cl st plst, n1, n2)
+  | StopAndGo (plst, n) -> StopAndGo (eval_plst cl st plst, n)
 
 (* Outermost function that is called by main.ml *)
-let eval (clist, alist, ret) = eval_pol alist clist ret
+let eval (cl, alst, ret) = eval_pol cl alst ret
