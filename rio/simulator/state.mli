184654fb206a286b6bb34dd58c600d7d89f59afc
Kabir Samsi <samsikabir@gmail.com>
2024-10-27 23:39:18 -0400

    More fixes to frontend simulator with AST fixes

--- rio/simulator/state.mli
+++ rio/simulator/state.mli
@@ -4,7 +4,7 @@ exception UnboundKey of string
 
 val empty : t
 val rebind : string -> float -> t -> t
-val rebind_all : (string * float) list -> t -> t
+val rebind_all : (string * 'a) list -> t -> t
 val is_defined : string -> t -> bool
 val lookup : string -> t -> float
 val lookup_opt : string -> t -> float option
