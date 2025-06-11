866adb81c9da4f987e99e0ce7cef4f178e07693c
polybeandip <akashdhiraj2019@gmail.com>
2025-06-11 17:15:46 -0400

    Add back IP layer
    - I am stupid

--- pcaps/pcap_gen.py
+++ pcaps/pcap_gen.py
@@ -1,5 +1,5 @@
 import os
-from scapy.all import Ether, Raw, wrpcap
+from scapy.all import Ether, IP, Raw, wrpcap
 
 a = "10:10:10:10:10:10"
 b = "20:20:20:20:20:20"
@@ -21,7 +21,7 @@ def three_flows():
     packets = []
     srcs = [a, b, c] * 3 + [a]
     for i, src in enumerate(srcs):
-        pkt = Ether(src=src, dst=dummy)
+        pkt = Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
         pkt.time = i / 10
         packets += pkt
     return packets
@@ -31,7 +31,7 @@ def three_flows_bursty():
     packets = []
     srcs = [a, b, c] * 16 + [a, b]
     for i, src in enumerate(srcs):
-        pkt = Ether(src=src, dst=dummy)
+        pkt = Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
         pkt.time = (i / 10) + int(i / 5)
         packets += pkt
     return packets
@@ -41,7 +41,7 @@ def two_then_three():
     packets = []
     srcs = [b, c] * 7 + [a] * 6 + [a, b, c] * 10
     for i, src in enumerate(srcs):
-        pkt = Ether(src=src, dst=dummy)
+        pkt = Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
         pkt.time = i / 10
         packets += pkt
     return packets
@@ -51,7 +51,7 @@ def four_flows():
     packets = []
     srcs = [a, b, c, d] * 12 + [a, b]
     for i, src in enumerate(srcs):
-        pkt = Ether(src=src, dst=dummy)
+        pkt = Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
         pkt.time = i / 10
         packets += pkt
     return packets
@@ -61,7 +61,7 @@ def five_flows():
     packets = []
     srcs = [a, b, c, d, e] * 10
     for i, src in enumerate(srcs):
-        pkt = Ether(src=src, dst=dummy)
+        pkt = Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
         pkt.time = i / 10
         packets += pkt
     return packets
@@ -71,7 +71,7 @@ def seven_flows():
     packets = []
     srcs = [a, b, c, d, e, f, g] * 7 + [a]
     for i, src in enumerate(srcs):
-        pkt = Ether(src=src, dst=dummy)
+        pkt = Ether(src=src, dst=dummy) / IP(src="1.1.1.1", dst="1.1.1.1")
         pkt.time = i / 10
         packets += pkt
     return packets
