9e7e33dd08871028db8c6fcf9d1facdcb8d7c760
polybeandip <akashdhiraj2019@gmail.com>
2025-06-25 16:31:29 -0400

    Fix dual push

--- pifo-design/flow_scheduler.sv
+++ pifo-design/flow_scheduler.sv
@@ -1,7 +1,7 @@
 `ifndef FLOW_SCHEDULER
 `define FLOW_SCHEDULER
 
-
+//                     requires  N > 1
 module FlowScheduler #(parameter N = 10) (
     input  clk,
     input  rst,
@@ -16,14 +16,15 @@ module FlowScheduler #(parameter N = 10) (
     input  [31:0] push_rank_2,
     input  [31:0] push_value_2,
 
-    output        is_full,
+    output        can_push_1,
+    output        can_push_2,
 
     // For dequeue
 
     input         pop,
     output [31:0] pop_value,
     output        pop_valid,
-    output        is_empty
+    output        can_pop
 );
 
     typedef struct {
@@ -35,8 +36,9 @@ module FlowScheduler #(parameter N = 10) (
     element elements [N - 1 : 0];
     logic [31:0] size;
 
-    assign is_full  = size == N;
-    assign is_empty = size == 0;
+    assign can_push_1 = size < N;
+    assign can_push_2 = size < N - 1;
+    assign can_pop    = size != 0;
 
     //-------------------------------------------------------------------------
     // Check stage
@@ -67,10 +69,10 @@ module FlowScheduler #(parameter N = 10) (
         end
 
         for ( int c = 0; c < 2; c++ ) begin
-            for ( int s = 0; s < 2; s++ )
-                if (  push_valid_S[s] &&
-                    ( insert_idx_S[s] > insert_idx_C[c] ||
-                    ( insert_idx_S[s] == insert_idx_C[c] && push_rank_S[s] <= push_rank_C[c] ) ) )
+            for ( int s = 1; s >= 0; s-- )
+                if ( push_valid_S[s] &&
+                   ( insert_idx_S[s] >  insert_idx_C[c] ||
+                   ( insert_idx_S[s] == insert_idx_C[c] && push_rank_S[s] <= push_rank_C[c] )))
                     insert_idx_C[c] = insert_idx_C[c] << 1;
 
             if ( pop_valid_S && !insert_idx_C[c][0] )
@@ -97,9 +99,9 @@ module FlowScheduler #(parameter N = 10) (
             for ( int i = 0; i < N; i++ ) elements[i].valid <= 0;
         end
         else begin
+            // swap if push_2's rank < push_1's rank
             for ( int i = 0; i < 2; i++ ) begin
-                if ( insert_idx_C[0] > insert_idx_C[1] && 
-                     push_valid_C[0] && push_valid_C[1] ) begin
+                if ( push_valid_C[1] && push_rank_C[1] <  push_rank_C[0] ) begin
                     insert_idx_S[i] <= insert_idx_C[1 - i];
                     push_value_S[i] <= push_value_C[1 - i];
                     push_rank_S[i]  <= push_rank_C[1 - i];
@@ -112,58 +114,43 @@ module FlowScheduler #(parameter N = 10) (
             end
 
             push_valid_S <= push_valid_C;
-            pop_valid_S <= pop_valid_C;
+            pop_valid_S  <= pop_valid_C;
+             
+            if ( push_valid_C[0] && push_valid_C[1] ) size <= size + 2;
+            else if ( push_valid_C[0] ) size <= size + 1;
+            if ( pop_valid_C ) size <= size - 1;
         end
     end
     
     // insert + shift
     always_ff @( posedge clk ) begin 
-        if ( push_valid_S[0] && push_valid_S[1] ) begin // perform two pushes 
+        // perform push 2
+        if ( push_valid_S[1] ) begin 
             // shifting
-            for ( int i = 0; i < N - 1; i++ ) begin
-                if ( i < N - 2 && insert_idx_S[0][i] && insert_idx_S[1][i] ) 
-                    elements[i + 2] <= elements[i];
-                else if ( insert_idx_S[0][i] || insert_idx_S[1][i] )
-                    elements[i + 1] <= elements[i];
+            for ( int i = 0; i < N - 2; i++ ) begin
+                if ( insert_idx_S[1][i] ) elements[i + 2] <= elements[i];
             end
             
             // insert
-            for ( int i = 1; i < N; i++ ) begin
-                if ( insert_idx_S[0] == insert_idx_S[1] ) begin
-                    if ( insert_idx_S[0][i] && !insert_idx_S[0][i - 1] ) begin
-                        elements[i].valid <= 1;
-                        elements[i].value <= 
-                            push_rank_S[0] <= push_rank_S[1] ? push_value_S[0] : push_value_S[1];
-                        elements[i].rank  <= 
-                            push_rank_S[0] <= push_rank_S[1] ? push_rank_S[0] : push_rank_S[1];
-
-                        elements[i + 1].valid <= 1;
-                        elements[i + 1].value <= 
-                            push_rank_S[0] <= push_rank_S[1] ? push_value_S[1] : push_value_S[0];
-                        elements[i + 1].rank  <= 
-                            push_rank_S[0] <= push_rank_S[1] ? push_rank_S[1] : push_rank_S[0];
-                    end
+            for ( int i = 1; i < N; i++ )
+                if ( insert_idx_S[1][i] && !insert_idx_S[1][i - 1] ) begin
+                    elements[i + 1].valid <= 1;
+                    elements[i + 1].value <= push_value_S[1];
+                    elements[i + 1].rank  <= push_rank_S[1];
                 end
-                else begin
-                    if ( insert_idx_S[0][i] && !insert_idx_S[0][i - 1] ) begin
-                        elements[i].valid <= 1;
-                        elements[i].value <= push_value_S[0];
-                        elements[i].rank  <= push_rank_S[0];
-                    end
-                    if ( insert_idx_S[1][i] && !insert_idx_S[1][i - 1] ) begin
-                        elements[i + 1].valid <= 1;
-                        elements[i + 1].value <= push_value_S[1];
-                        elements[i + 1].rank  <= push_rank_S[1];
-                    end
-                end
+            if ( insert_idx_S[1][0] == 1 ) begin
+                elements[1].valid <= 1;
+                elements[1].value <= push_value_S[1];
+                elements[1].rank  <= push_rank_S[1];
             end
-
-            size <= size + 2;
         end
-        else if ( push_valid_S[0] ) begin // perform push
+
+        // perform push 1
+        if ( push_valid_S[0] ) begin 
             // shift
             for ( int i = 0; i < N - 1; i++ ) begin
-                if ( insert_idx_S[0][i] ) elements[i + 1] <= elements[i];
+                if ( insert_idx_S[0][i] && ( !push_valid_S[1] || !insert_idx_S[1][i]) )
+                    elements[i + 1] <= elements[i];
             end
 
             // insert
@@ -178,15 +165,13 @@ module FlowScheduler #(parameter N = 10) (
                 elements[0].value <= push_value_S[0];
                 elements[0].rank  <= push_rank_S[0];
             end
-
-            size <= size + 1;
         end
-        else if ( pop_valid_S ) begin // perform pop
+
+        // perform pop
+        if ( pop_valid_S ) begin 
             // shift
             elements[N - 1].valid <= 0;
             for ( int i = 1; i < N; i++ ) elements[i - 1] <= elements[i];
-
-            size <= size - 1;
         end
     end
     
