c67ef4cb9425b326c16400cb76ea487781def84c
Anshuman Mohan <anshumanmohan@live.com>
2026-01-21 10:52:51 -0500

    Annotate basic example

--- pifo-hardware/hw/spinal/rio/sim/PifoMeshSim.scala
+++ pifo-hardware/hw/spinal/rio/sim/PifoMeshSim.scala
@@ -25,7 +25,7 @@ object PifoMeshSim extends App {
       controller.start
 
       import RioPredefinedPifos._
-      // Once you import this, rPifo(0) is 0xA... rPifo(5) is 0xF.
+      // Once you import this, rPifo(0) is 0xA .. rPifo(5) is 0xF.
 
       val engine1 = 1
       val engine2 = 2
@@ -33,18 +33,19 @@ object PifoMeshSim extends App {
       val tree1 = TreeController(
         controller,
         pifos = Seq((engine1, rPifo(0)), (engine2, rPifo(1)), (engine2, rPifo(2)))
+        // engine1 has pifo0, and engine2 has pifo1 and pifo2.
       )
 
       // use controller.transaction if you want transactional configuration
       val configThread = controller.config { cf =>
         cf.tree(tree1)
-          .addFlow(rFlow(0), Seq(rPifo(0), rPifo(1)))
-          .addFlow(rFlow(1), Seq(rPifo(0), rPifo(2)))
-          .brainSP(rPifo(0))
+          .addFlow(rFlow(0), Seq(rPifo(0), rPifo(1))) // flow0 is associated with BOTH pifo0 and pifo1
+          .addFlow(rFlow(1), Seq(rPifo(0), rPifo(2))) // flow1 is associated with BOTH pifo0 and pifo2
+          .brainSP(rPifo(0)) // pifo0 runs the SP policy
           .brainState(rPifo(0), rFlow(0), 10) // prio of flow0 in pifo0 -> 10
           .brainState(rPifo(0), rFlow(1), 20) // prio of flow1 in pifo0 -> 20
           .brainFIFO(rPifo(1))
-          .brainFIFO(rPifo(2))
+          .brainFIFO(rPifo(2)) // pifo1 and pifo2 run the FIFO policy
       }
 
       // join here to ensure configuration completes before proceeding
@@ -55,11 +56,16 @@ object PifoMeshSim extends App {
       dut.clockDomain.waitRisingEdge(4)
 
       for (i <- 0 until 3) {
-        controller.enque(rFlow(0))
+        controller.enque(
+          rFlow(0)
+        ) // this is a compound function that enqueues into all the pifos associated with the flow.
+        // AM question: true?
+        // AM question: but it's not in parallel? See Utils.scala.
         dut.clockDomain.waitRisingEdge(1)
         controller.enque(rFlow(1))
         dut.clockDomain.waitRisingEdge(1)
       }
+      // So a total of 6 items have been enqueued.
 
       dut.clockDomain.waitRisingEdge(6)
 
@@ -67,6 +73,8 @@ object PifoMeshSim extends App {
       for (_ <- 0 until 8) {
         tree1.deque
       }
+      // By doing 8 dequeues we will probably trigger underflow.
+      // AM question: true?
 
       dut.clockDomain.waitRisingEdge(20)
 
