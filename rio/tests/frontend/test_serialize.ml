d25b3f917dfa9c7d6349b93542f39cece64cc0a7
Anshuman Mohan <anshumanmohan@live.com>
2026-01-22 15:25:43 -0500

    Basic comparison b/w two ASTs

--- rio/tests/frontend/test_serialize.ml
+++ rio/tests/frontend/test_serialize.ml
@@ -17,7 +17,12 @@ let make_test name file =
     (prog_to_json file) ~printer:Yojson.Basic.pretty_to_string
     ~cmp:Yojson.Basic.equal
 
-let tests =
+let make_compare_test name file1 file2 expected =
+  name >:: fun _ ->
+  let result = Json.compare_programs prog_dir file1 file2 in
+  assert_equal expected result ~printer:string_of_bool
+
+let serialize_tests =
   [
     make_test "single class policy" "work_conserving/drop_a_class.sched";
     make_test "fifo 1 class" "work_conserving/fifo_1_class_sugar.sched";
@@ -34,5 +39,15 @@ let tests =
     make_test "complex tree" "work_conserving/complex_tree.sched";
   ]
 
-let suite = "serialization tests" >::: tests
+let compare_tests =
+  [
+    make_compare_test "same program twice" "work_conserving/fifo_1_class.sched"
+      "work_conserving/fifo_1_class.sched" true;
+    make_compare_test "different programs" "work_conserving/fifo_1_class.sched"
+      "work_conserving/rr_1_class.sched" false;
+    make_compare_test "rr 1 vs rr 2" "work_conserving/rr_1_class.sched"
+      "work_conserving/rr_2_classes.sched" false;
+  ]
+
+let suite = "serialization tests" >::: serialize_tests @ compare_tests
 let () = run_test_tt_main suite
