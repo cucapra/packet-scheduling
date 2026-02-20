f50f64aba469cb7c691153aeec856679e48482fe
Anshuman Mohan <anshumanmohan@live.com>
2026-02-20 15:14:12 -0500

    Carve comparison tests into own file

---
+++ rio/tests/frontend/test_compare.ml
@@ -0,0 +1,159 @@
+open Frontend
+open OUnit2
+open Rio_compare.Compare
+
+let root_dir = "../../../../../"
+let prog_dir = root_dir ^ "progs/"
+
+let prog_to_policy file =
+  prog_dir ^ file |> Parser.parse_file |> Policy.of_program
+
+let make_compare_test name file1 file2 expected_diff =
+  name >:: fun _ ->
+  let policy1 = prog_to_policy file1 in
+  let policy2 = prog_to_policy file2 in
+  let actual_diff = analyze policy1 policy2 in
+  assert_equal expected_diff actual_diff ~printer:(fun d ->
+      Rio_compare.Compare.to_string d)
+
+let compare_tests_same =
+  [
+    make_compare_test "same program twice"
+      "work_conserving/strict_3_classes.sched"
+      "work_conserving/strict_3_classes.sched" Same;
+    make_compare_test "merely jumbled in RR"
+      "work_conserving/rr_hier_merge_sugar.sched"
+      "work_conserving/rr_hier_merge_sugar_jumbled.sched" Same;
+    make_compare_test "merely jumbled in WFQ"
+      "work_conserving/wfq_3_classes.sched"
+      "work_conserving/wfq_3_classes_jumbled.sched" Same;
+    make_compare_test "different variable names, same structure"
+      "work_conserving/rr_hier.sched"
+      "work_conserving/rr_hier_weird_var_names.sched" Same;
+  ]
+
+let compare_tests_different =
+  [
+    (* SP(B,A) vs SP(C,B,A) *)
+    make_compare_test "strict arm added"
+      "work_conserving/strict_2_classes.sched"
+      "work_conserving/strict_3_classes.sched"
+      (Change
+         ( [],
+           ArmsAdded
+             { old_count = 2; new_count = 3; details = "added fifo[C] at 1" } ));
+    (* TODO: is this index okay? *)
+    (* SP(B,A) vs SP(B,C,A) *)
+    make_compare_test "strict arm added in the middle"
+      "work_conserving/strict_2_classes.sched"
+      "work_conserving/strict_3_classes_BCA.sched"
+      (Change
+         ( [],
+           ArmsAdded
+             { old_count = 2; new_count = 3; details = "added fifo[C] at 2" } ));
+    (* SP(B,A) vs SP(A,B,C) *)
+    make_compare_test "strict arm added whilst reordering arms"
+      "work_conserving/strict_2_classes.sched"
+      "work_conserving/strict_3_classes_ABC.sched"
+      (Change ([], VeryDifferent));
+    (* WFQ(A,B,C) vs WFQ(A,B,D) *)
+    make_compare_test "different WFQ" "work_conserving/wfq_3_classes.sched"
+      "work_conserving/wfq_3_classes_diff.sched"
+      (Change ([ 2 ], VeryDifferent));
+    (* RR(A,B) vs RR(A,B,C) *)
+    make_compare_test "RR with arm added" "work_conserving/rr_2_classes.sched"
+      "work_conserving/rr_3_classes.sched"
+      (Change
+         ( [],
+           ArmsAdded
+             { old_count = 2; new_count = 3; details = "added fifo[C] at 2" } ));
+    (* RR(A,B) vs RR(C,A,B) *)
+    make_compare_test "RR with arm added (CAB)"
+      "work_conserving/rr_2_classes.sched"
+      "work_conserving/rr_3_classes_CAB.sched"
+      (Change
+         ( [],
+           ArmsAdded
+             { old_count = 2; new_count = 3; details = "added fifo[C] at 2" } ));
+    (* WFQ(A,B) vs WFQ(A,B,C) *)
+    make_compare_test "WFQ with arm added" "work_conserving/wfq_2_classes.sched"
+      "work_conserving/wfq_3_classes.sched"
+      (Change
+         ( [],
+           ArmsAdded
+             {
+               old_count = 2;
+               new_count = 3;
+               details = "added fifo[C] at 2 with weight 3";
+             } ));
+    (* RR(A,B) vs RR(D,E,F) *)
+    make_compare_test "RR big diff" "work_conserving/rr_2_classes.sched"
+      "work_conserving/rr_3_classes_DEF.sched"
+      (Change ([], VeryDifferent));
+    (* SP(C,B,A) vs SP(B,C,A) *)
+    make_compare_test "Strict with arms reordered"
+      "work_conserving/strict_3_classes.sched"
+      "work_conserving/strict_3_classes_jumbled.sched"
+      (Change ([ 0 ], VeryDifferent));
+    (* WFQ weights changed *)
+    make_compare_test "WFQ with weights changed"
+      "work_conserving/wfq_3_classes.sched"
+      "work_conserving/wfq_3_classes_diff_weights.sched"
+      (Change ([], VeryDifferent));
+    make_compare_test "WFQ with weights changed and arm added"
+      "work_conserving/wfq_3_classes.sched"
+      "work_conserving/wfq_very_diff.sched"
+      (Change ([], VeryDifferent));
+    (* Test arm removal *)
+    make_compare_test "RR with arm removed" "work_conserving/rr_3_classes.sched"
+      "work_conserving/rr_2_classes.sched"
+      (Change ([], ArmsRemoved { old_count = 3; new_count = 2 }));
+    make_compare_test "WFQ with arm removed"
+      "work_conserving/wfq_3_classes.sched"
+      "work_conserving/wfq_2_classes.sched"
+      (Change ([], ArmsRemoved { old_count = 3; new_count = 2 }));
+  ]
+
+let compare_tests_superpol =
+  [
+    make_compare_test "B is sub-pol of large tree"
+      "work_conserving/FIFO_B.sched" "work_conserving/rr_hier_superpol.sched"
+      (Change ([ 1; 0 ], SuperPol));
+    make_compare_test "sub-policy of rr_hier"
+      "work_conserving/rr_hier_subpol.sched" "work_conserving/rr_hier.sched"
+      (Change ([ 1 ], SuperPol));
+    make_compare_test "rr_hier is subpol of large tree"
+      "work_conserving/rr_hier_subpol.sched"
+      "work_conserving/rr_hier_superpol.sched"
+      (Change ([ 1; 1 ], SuperPol));
+  ]
+
+let compare_tests_deep =
+  [
+    make_compare_test "RR/Strict hierarchy with arm added deep"
+      "work_conserving/rr_strict_hier.sched"
+      "work_conserving/rr_strict_hier_add_arm.sched"
+      (Change
+         ( [ 2; 0 ],
+           ArmsAdded
+             { old_count = 2; new_count = 3; details = "added fifo[CY] at 3" }
+         ));
+    make_compare_test "RR/Strict hierarchy with RR swap deep"
+      "work_conserving/rr_strict_hier.sched"
+      "work_conserving/rr_strict_hier_swap_deep_1.sched" Same;
+    make_compare_test "RR/Strict hierarchy with SP swap deep"
+      "work_conserving/rr_strict_hier.sched"
+      "work_conserving/rr_strict_hier_swap_deep_2.sched"
+      (Change ([ 2; 0; 0 ], VeryDifferent));
+    make_compare_test "RR/Strict hierarchy with arm removed deep"
+      "work_conserving/rr_strict_hier_add_arm.sched"
+      "work_conserving/rr_strict_hier.sched"
+      (Change ([ 2; 0 ], ArmsRemoved { old_count = 3; new_count = 2 }));
+  ]
+
+let suite =
+  "compare tests"
+  >::: compare_tests_same @ compare_tests_different @ compare_tests_superpol
+       @ compare_tests_deep
+
+let () = run_test_tt_main suite
--- rio/tests/frontend/test_serialize.ml
+++ rio/tests/frontend/test_serialize.ml
@@ -8,9 +8,6 @@ let json_dir = "jsons/"
 let prog_to_json file =
   prog_dir ^ file |> Parser.parse_file |> Policy.of_program |> Policy.to_json
 
-let prog_to_policy file =
-  prog_dir ^ file |> Parser.parse_file |> Policy.of_program
-
 let make_test name file =
   let base = file |> Filename.basename |> Filename.remove_extension in
   let json = json_dir ^ Printf.sprintf "%s.json" base in
@@ -19,14 +16,6 @@ let make_test name file =
     (Yojson.Basic.from_file json)
     (prog_to_json file) ~printer:Yojson.Basic.pretty_to_string
     ~cmp:Yojson.Basic.equal
-
-let make_compare_test name file1 file2 expected_diff =
-  name >:: fun _ ->
-  let policy1 = prog_to_policy file1 in
-  let policy2 = prog_to_policy file2 in
-  let actual_diff = Rio_compare.Compare.analyze policy1 policy2 in
-  assert_equal expected_diff actual_diff ~printer:(fun d ->
-      Rio_compare.Compare.to_string d)
 
 let serialize_tests =
   [
@@ -46,148 +35,5 @@ let serialize_tests =
     make_test "complex tree" "work_conserving/complex_tree.sched";
   ]
 
-let compare_tests_same =
-  let open Rio_compare.Compare in
-  [
-    make_compare_test "same program twice"
-      "work_conserving/strict_3_classes.sched"
-      "work_conserving/strict_3_classes.sched" Same;
-    make_compare_test "merely jumbled in RR"
-      "work_conserving/rr_hier_merge_sugar.sched"
-      "work_conserving/rr_hier_merge_sugar_jumbled.sched" Same;
-    make_compare_test "merely jumbled in WFQ"
-      "work_conserving/wfq_3_classes.sched"
-      "work_conserving/wfq_3_classes_jumbled.sched" Same;
-    make_compare_test "different variable names, same structure"
-      "work_conserving/rr_hier.sched"
-      "work_conserving/rr_hier_weird_var_names.sched" Same;
-  ]
-
-let compare_tests_different =
-  let open Rio_compare.Compare in
-  [
-    (* SP(B,A) vs SP(C,B,A) *)
-    make_compare_test "strict arm added"
-      "work_conserving/strict_2_classes.sched"
-      "work_conserving/strict_3_classes.sched"
-      (Change
-         ( [],
-           ArmsAdded
-             { old_count = 2; new_count = 3; details = "added fifo[C] at 1" } ));
-    (* TODO: is this index okay? *)
-    (* SP(B,A) vs SP(B,C,A) *)
-    make_compare_test "strict arm added in the middle"
-      "work_conserving/strict_2_classes.sched"
-      "work_conserving/strict_3_classes_BCA.sched"
-      (Change
-         ( [],
-           ArmsAdded
-             { old_count = 2; new_count = 3; details = "added fifo[C] at 2" } ));
-    (* SP(B,A) vs SP(A,B,C) *)
-    make_compare_test "strict arm added whilst reordering arms"
-      "work_conserving/strict_2_classes.sched"
-      "work_conserving/strict_3_classes_ABC.sched"
-      (Change ([], VeryDifferent));
-    (* WFQ(A,B,C) vs WFQ(A,B,D) *)
-    make_compare_test "different WFQ" "work_conserving/wfq_3_classes.sched"
-      "work_conserving/wfq_3_classes_diff.sched"
-      (Change ([ 2 ], VeryDifferent));
-    (* RR(A,B) vs RR(A,B,C) *)
-    make_compare_test "RR with arm added" "work_conserving/rr_2_classes.sched"
-      "work_conserving/rr_3_classes.sched"
-      (Change
-         ( [],
-           ArmsAdded
-             { old_count = 2; new_count = 3; details = "added fifo[C] at 2" } ));
-    (* RR(A,B) vs RR(C,A,B) *)
-    make_compare_test "RR with arm added (CAB)"
-      "work_conserving/rr_2_classes.sched"
-      "work_conserving/rr_3_classes_CAB.sched"
-      (Change
-         ( [],
-           ArmsAdded
-             { old_count = 2; new_count = 3; details = "added fifo[C] at 2" } ));
-    (* WFQ(A,B) vs WFQ(A,B,C) *)
-    make_compare_test "WFQ with arm added" "work_conserving/wfq_2_classes.sched"
-      "work_conserving/wfq_3_classes.sched"
-      (Change
-         ( [],
-           ArmsAdded
-             {
-               old_count = 2;
-               new_count = 3;
-               details = "added fifo[C] at 2 with weight 3";
-             } ));
-    (* RR(A,B) vs RR(D,E,F) *)
-    make_compare_test "RR big diff" "work_conserving/rr_2_classes.sched"
-      "work_conserving/rr_3_classes_DEF.sched"
-      (Change ([], VeryDifferent));
-    (* SP(C,B,A) vs SP(B,C,A) *)
-    make_compare_test "Strict with arms reordered"
-      "work_conserving/strict_3_classes.sched"
-      "work_conserving/strict_3_classes_jumbled.sched"
-      (Change ([ 0 ], VeryDifferent));
-    (* WFQ weights changed *)
-    make_compare_test "WFQ with weights changed"
-      "work_conserving/wfq_3_classes.sched"
-      "work_conserving/wfq_3_classes_diff_weights.sched"
-      (Change ([], VeryDifferent));
-    make_compare_test "WFQ with weights changed and arm added"
-      "work_conserving/wfq_3_classes.sched"
-      "work_conserving/wfq_very_diff.sched"
-      (Change ([], VeryDifferent));
-    (* Test arm removal *)
-    make_compare_test "RR with arm removed" "work_conserving/rr_3_classes.sched"
-      "work_conserving/rr_2_classes.sched"
-      (Change ([], ArmsRemoved { old_count = 3; new_count = 2 }));
-    make_compare_test "WFQ with arm removed"
-      "work_conserving/wfq_3_classes.sched"
-      "work_conserving/wfq_2_classes.sched"
-      (Change ([], ArmsRemoved { old_count = 3; new_count = 2 }));
-  ]
-
-let compare_tests_superpol =
-  let open Rio_compare.Compare in
-  [
-    make_compare_test "B is sub-pol of large tree"
-      "work_conserving/FIFO_B.sched" "work_conserving/rr_hier_superpol.sched"
-      (Change ([ 1; 0 ], SuperPol));
-    make_compare_test "sub-policy of rr_hier"
-      "work_conserving/rr_hier_subpol.sched" "work_conserving/rr_hier.sched"
-      (Change ([ 1 ], SuperPol));
-    make_compare_test "rr_hier is subpol of large tree"
-      "work_conserving/rr_hier_subpol.sched"
-      "work_conserving/rr_hier_superpol.sched"
-      (Change ([ 1; 1 ], SuperPol));
-  ]
-
-let compare_tests_deep =
-  let open Rio_compare.Compare in
-  [
-    make_compare_test "RR/Strict hierarchy with arm added deep"
-      "work_conserving/rr_strict_hier.sched"
-      "work_conserving/rr_strict_hier_add_arm.sched"
-      (Change
-         ( [ 2; 0 ],
-           ArmsAdded
-             { old_count = 2; new_count = 3; details = "added fifo[CY] at 3" }
-         ));
-    make_compare_test "RR/Strict hierarchy with RR swap deep"
-      "work_conserving/rr_strict_hier.sched"
-      "work_conserving/rr_strict_hier_swap_deep_1.sched" Same;
-    make_compare_test "RR/Strict hierarchy with SP swap deep"
-      "work_conserving/rr_strict_hier.sched"
-      "work_conserving/rr_strict_hier_swap_deep_2.sched"
-      (Change ([ 2; 0; 0 ], VeryDifferent));
-    make_compare_test "RR/Strict hierarchy with arm removed deep"
-      "work_conserving/rr_strict_hier_add_arm.sched"
-      "work_conserving/rr_strict_hier.sched"
-      (Change ([ 2; 0 ], ArmsRemoved { old_count = 3; new_count = 2 }));
-  ]
-
-let suite =
-  "serialization tests"
-  >::: serialize_tests @ compare_tests_same @ compare_tests_different
-       @ compare_tests_superpol @ compare_tests_deep
-
+let suite = "serialization tests" >::: serialize_tests
 let () = run_test_tt_main suite
