176a908e460b547a3050836fec6af2660363da27
Anshuman Mohan <10830208+anshumanmohan@users.noreply.github.com>
2026-02-24 12:52:25 -0500

    More sugar, and trimming some fat from the testing suite (#88)
    Our test suite was getting hard to use in a few ways:
    - There were massively redundant tests
    - There was identical files with names like `foo` and `foo_sugar`
    - Naming was poor
    - Not using syntactic sugar had made the `.sched` files annoying to read
    
    This PR fixes a bunch of that. It adds a little sugar to the parser, and
    then does a bunch of housekeeping besides. It obviates and closes #87.

--- rio/tests/frontend/test_parsing.ml
+++ rio/tests/frontend/test_parsing.ml
@@ -16,30 +16,19 @@ let make_error_test name filename exn =
 
 let wc_tests =
   [
-    make_test "single class policy" "work_conserving/drop_a_class.sched"
-      "fifo[A]";
-    make_test "fifo 1 class" "work_conserving/fifo_1_class_sugar.sched"
-      "fifo[A]";
-    make_test "fifo 1 class" "work_conserving/fifo_1_class.sched" "fifo[A]";
-    make_test "fifo of 3" "work_conserving/fifo_n_classes.sched"
+    make_test "single class policy" "work_conserving/drop_class.sched" "fifo[A]";
+    make_test "fifo 1 class" "work_conserving/fifo_A.sched" "fifo[A]";
+    make_test "union of 3" "work_conserving/union_ABC.sched"
       "fifo[union[A, B, C]]";
-    make_test "rr of 1" "work_conserving/rr_1_class.sched" "rr[fifo[A]]";
-    make_test "rr of 2" "work_conserving/rr_2_classes.sched"
-      "rr[fifo[union[A, B]]]";
-    make_test "multiple assignments" "work_conserving/rr_hier_merge_sugar.sched"
+    make_test "multiple assignments" "work_conserving/rr_union_hier.sched"
       "rr[fifo[union[BX, BY]], rr[fifo[RP], fifo[RT]]]";
-    make_test "2 assignments w/ substitutions" "work_conserving/rr_hier.sched"
-      "rr[fifo[B], rr[fifo[RP], fifo[RT]]]";
-    make_test "3 classes with substitutions"
-      "work_conserving/rr_n_class_hier.sched"
-      "rr[fifo[A], fifo[B], rr[rr[fifo[CU], fifo[CV]], rr[fifo[CW], fifo[CX]]]]";
-    make_test "rr of 3" "work_conserving/rr_n_classes.sched"
+    make_test "rr of 3" "work_conserving/rr_ABC.sched"
       "rr[fifo[A], fifo[B], fifo[C]]";
     make_test "rr and strict substitutions"
-      "work_conserving/rr_strict_n_classes_hier.sched"
+      "work_conserving/rr_strict_hier.sched"
       "strict[fifo[A], fifo[B], rr[rr[fifo[CU], fifo[CV]], strict[fifo[CW], \
        fifo[CX]]]]";
-    make_test "strict of 3" "work_conserving/strict_n_classes.sched"
+    make_test "strict of 3" "work_conserving/strict_CBA.sched"
       "strict[fifo[C], fifo[B], fifo[A]]";
   ]
 
@@ -60,22 +49,17 @@ let _nwc_tests =
 
 let error_tests =
   [
-    make_error_test "undeclared class" "incorrect/undeclared_classes.sched"
+    make_error_test "undeclared class" "incorrect/unbound_class.sched"
       (Policy.UndeclaredClass "Z");
-    make_error_test "unbound variable" "incorrect/unbound_var.sched"
-      (Policy.UnboundVariable "policy");
     make_error_test "unbound var in middle of list of assignments"
-      "incorrect/unbound_var_hier.sched" (Policy.UnboundVariable "r_polic");
+      "incorrect/unbound_var.sched" (Policy.UnboundVariable "r_police");
     make_error_test "class used twice in policy"
       "incorrect/duplicate_classes.sched" (Policy.DuplicateClass "B");
     make_error_test "class used twice in one fifo"
       "incorrect/duplicate_samepol.sched" (Policy.DuplicateClass "A");
     make_error_test "fifo for multiple classes without union"
-      "incorrect/set_multiple.sched"
+      "incorrect/fifo_multiple.sched"
       (Parser.ParserError { row = Some 4; col = Some 17; char = None });
-    make_error_test "rr for classes without fifo'ing first"
-      "incorrect/set_hierarchical.sched"
-      (Parser.ParserError { row = Some 3; col = Some 14; char = None });
   ]
 
 let suite = "parsing tests" >::: wc_tests @ error_tests (* @ nwc_tests *)
--- rio/tests/frontend/test_serialize.ml
+++ rio/tests/frontend/test_serialize.ml
@@ -20,18 +20,13 @@ let make_test name file =
 
 let tests =
   [
-    make_test "single class policy" "work_conserving/drop_a_class.sched";
-    make_test "fifo 1 class" "work_conserving/fifo_1_class_sugar.sched";
-    make_test "fifo 2 class union" "work_conserving/fifo_2_class_union.sched";
-    make_test "fifo n classes" "work_conserving/fifo_n_classes.sched";
-    make_test "rr 1 class" "work_conserving/rr_1_class.sched";
-    make_test "rr 2 classes" "work_conserving/rr_2_classes.sched";
-    make_test "rr hierarchy" "work_conserving/rr_hier_merge_sugar.sched";
-    make_test "rr even hierarchy" "work_conserving/rr_hier.sched";
-    make_test "rr n classes hierarchy" "work_conserving/rr_n_class_hier.sched";
-    make_test "rr n classes" "work_conserving/rr_n_classes.sched";
-    make_test "rr and strict" "work_conserving/rr_strict_n_classes_hier.sched";
-    make_test "strict n classes" "work_conserving/strict_n_classes.sched";
+    make_test "single class policy" "work_conserving/drop_class.sched";
+    make_test "fifo 1 class" "work_conserving/fifo_A.sched";
+    make_test "union ABC" "work_conserving/union_ABC.sched";
+    make_test "rr hierarchy" "work_conserving/rr_union_hier.sched";
+    make_test "rr n classes" "work_conserving/rr_ABC.sched";
+    make_test "rr and strict" "work_conserving/rr_strict_hier.sched";
+    make_test "strict n classes" "work_conserving/strict_CBA.sched";
     make_test "complex tree" "work_conserving/complex_tree.sched";
   ]
 
